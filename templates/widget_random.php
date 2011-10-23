6ea0f5c722e740bee267012adf649ff8a749847d
Doug Bromley <doug@tintophat.com>
2011-10-24 00:22:45 +0100

    Adding price to random widget

--- templates/widget_random.php
+++ templates/widget_random.php
@@ -3,10 +3,11 @@
 <?php $name_parts = explode(' ', $row->name); ?>
     <li>
         <a href="/hopo/<?php echo $row->id; ?>">
-            <img src="<?php echo $row->aw_thumb; ?>" alt="<?php echo $row->name; ?>" style="padding:5px;float:left;width:40px;" />
+            <img src="<?php echo $row->aw_thumb; ?>" alt="<?php echo $row->name; ?>" style="padding:5px;float:left;width:50px;" />
             <?php echo "{$name_parts[0]} {$name_parts[1]} {$name_parts[2]}"; ?>...
         </a>
-        <br style="clear:both;" />
+        <div style="margin:0px;padding:0px;">&pound;<?php echo $row->price; ?></div>
+        <div style="clear:both;"></div>
     </li>
 <?php endforeach; ?>
 </ul>
